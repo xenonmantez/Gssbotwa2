28e99d650c5216bbe9cdb6afc68d116a90f8402e
xenonmantez <mghosiduncan23@gmail.com>
2024-08-06 19:19:52 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
+global.ownername = "xenon";
 global.owner = ['917050906659'];
 global.premium = ['917050906659'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
@@ -24,7 +24,7 @@ global.autoRecord = false
 global.autoreact = false
 global.onlygroup = false
 global.onlypc = false
-global.welcome = true
+global.welcome = false
 global.autoBlock = true
  
 /*REPLY MESSAGE*/
